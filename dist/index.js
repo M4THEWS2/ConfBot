c140e5022c4fe62b1c0f936f654dbed10362983c
M4THEWS2 <m4thews043@gmail.com>
2021-08-16 13:29:52 -0300

    #

--- dist/index.js
+++ dist/index.js
@@ -3,3 +3,4 @@ var Discord = require('discord.js');
 var config = require('../config.json');
 var client = new Discord.Client({ intents: ["GUILDS", "GUILD_MESSAGES"] });
 var token = config.token;
+client;
