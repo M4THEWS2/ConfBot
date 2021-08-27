6c8270360fab3a3b53c0ed31e7b44a33fb587287
M4THEWS2 <m4thews043@gmail.com>
2021-08-27 11:12:03 -0300

    .

--- src/commands/send.js
+++
@@ -1,7 +0,0 @@
-const Discord = require('discord.js');
-const config = require('../../config.json');
-const getVariables = require('../functions/getVariables.js');
-
-module.exports = function (client, msg, args, command) {
-    msg.channel.send(getVariables(command.content, msg));
-}
