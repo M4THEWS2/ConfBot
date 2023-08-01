4436fcf85c4f8b7f0280c6307946146bf52838d5
Matheus Cristian <MatheusCristianRG@gmail.com>
2023-08-01 00:48:08 -0300

    Change things so my spell checker stop warning me

--- src/utils.ts
+++ src/utils.ts
@@ -1,4 +1,4 @@
-export const errorPrefix = "\x1b[31merror: \x1b[0m";
+export const warnPrefix = "\x1b[33m" + "warn: \x1b[0m";
+export const infoPrefix = "\x1b[36m" + "info: \x1b[0m";
 
-export const warnPrefix = "\x1b[33mwarn: \x1b[0m";
-export const infoPrefix = "\x1b[36minfo: \x1b[0m";
+export const errorPrefix = "\x1b[31m" + "error: \x1b[0m";
